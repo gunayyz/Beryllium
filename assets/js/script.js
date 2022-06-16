7e04c6ec1c9691f65df5a6ffcc0c4358eb1d19d1
gunayyz <7cgq295@code.edu.az>
2022-06-16 18:38:49 +0400

    m

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $('.workphotos').slick({
-  dots: true,
+  dots: false,
   infinite: false,
   speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 3,
+  slidesToScroll: 3,
   responsive: [
     {
       breakpoint: 1024,
@@ -11,7 +11,7 @@ $('.workphotos').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
-        dots: true
+        dots: false
       }
     },
     {
